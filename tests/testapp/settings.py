5fe436952784ef598ad22f538ce7330e81e395e8
Artem <tyomo4ka@users.noreply.github.com>
2020-11-10 23:59:48 +1100

    Use postgres for testapp. (#19)
    Co-authored-by: Artem <artem@city2code.com>

--- tests/testapp/settings.py
+++ tests/testapp/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": 5432,
     }
 }
 
